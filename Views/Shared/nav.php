76a965c23ac1e8ed8f4add71b14b68d9f443b9f1
diomarmajano <http.diomar@gmail.com>
2024-05-24 12:56:36 -0400

    Se agregó el archivo route.php y se actualizaron las llamadas de vista en los nav

--- Views/Shared/nav.php
+++ Views/Shared/nav.php
@@ -1,3 +1,6 @@
+<?php
+include __DIR__ .'/../../route.php';
+?>
 <header class="navbar navbar-light fixed-top" style="background-color: #023059;">
 <div class="nav-container">
         <a class="navbar-brand" href="../index.php">
@@ -6,22 +9,22 @@
     <nav class="nav-ul">
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link text-blue" href="mantenedorusuarios.php" style="color:#FFFFFF"> <i class="fa-regular fa-user" style="color: #FFFFFF;"></i> Usuarios</a>
+                <a class="nav-link text-blue" href="<?= USUARIOS ?>" style="color:#FFFFFF"> <i class="fa-regular fa-user" style="color: #FFFFFF;"></i> Usuarios</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue" href="mantenedordiagnostico.php" style="color:#FFFFFF"><i class="fa-solid fa-microscope" style="color: #FFFFFF;"></i> Diagnostico</a>
+                <a class="nav-link text-blue" href="<?= DIAGNOSTICO ?>" style="color:#FFFFFF"><i class="fa-solid fa-microscope" style="color: #FFFFFF;"></i> Diagnostico</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue" href="mantenedorlaboratorios.php" style="color:#FFFFFF"><i class="fa-regular fa-hospital" style="color: #FFFFFF;"></i>Centros Médicos</a>
+                <a class="nav-link text-blue" href="<?= LABORATORIOS ?>" style="color:#FFFFFF"><i class="fa-regular fa-hospital" style="color: #FFFFFF;"></i>Centros Médicos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue" href="mantenedorestados.php" style="color:#FFFFFF"><i class="fa-solid fa-bars" style="color: #FFFFFF;"></i> Estados</a>
+                <a class="nav-link text-blue" href="<?= ESTADOS ?>" style="color:#FFFFFF"><i class="fa-solid fa-bars" style="color: #FFFFFF;"></i> Estados</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue" href="mantenedorPerfiles.php" style="color:#FFFFFF"><i class="fa-solid fa-users" style="color: #FFFFFF;"></i> Perfiles</a>
+                <a class="nav-link text-blue" href="<?= PERFILES ?>" style="color:#FFFFFF"><i class="fa-solid fa-users" style="color: #FFFFFF;"></i> Perfiles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue"  href="cerrarsesion.php" style="color:#FFFFFF"> <i class="fa-solid fa-right-from-bracket" style="color: #FFFFFF;"></i> Cerrar Sesi&oacuten</a>
+                <a class="nav-link text-blue"  href="<?= LOGOUT ?>" style="color:#FFFFFF"> <i class="fa-solid fa-right-from-bracket" style="color: #FFFFFF;"></i> Cerrar Sesi&oacuten</a>
             </li>
         </ul>
     </nav>
