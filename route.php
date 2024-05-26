299d323c1bf8aad9efaa5732142d4dde691ba432
diomarmajano <http.diomar@gmail.com>
2024-05-26 15:38:56 -0400

    agregar rutas

---
+++ route.php
@@ -0,0 +1,11 @@
+<?php
+const USUARIOS = 'mantenedorusuarios.php';
+const DIAGNOSTICO = 'mantenedordiagnostico.php';
+const LABORATORIOS = 'mantenedorlaboratorios.php';
+const ESTADOS = 'mantenedorestados.php';
+const PERFILES = 'mantenedorPerfiles.php';
+const LOGOUT = 'cerrarsesion.php';
+const RECEPTION = 'recepcion.php';
+const REGISTRO = 'registro.php';
+
+const DIAGNOSTICOS = 'diagnostico.php';
