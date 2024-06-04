8fe8eca858c646d6d18239408333fb92ee1d5d01
kv.rodriguezgarcia@gmail.com <kv.rodriguezgarcia@gmail.com>
2024-06-04 12:51:24 -0400

    Se agrega reporte de examenes

--- Views/Shared/nav.php
+++ Views/Shared/nav.php
@@ -24,7 +24,10 @@ include __DIR__ .'/../../route.php';
                 <a class="nav-link text-blue" href="<?= PERFILES ?>" style="color:#FFFFFF"><i class="fa-solid fa-users" style="color: #FFFFFF;"></i> Perfiles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-blue"  href="<?= LOGOUT ?>" style="color:#FFFFFF"> <i class="fa-solid fa-right-from-bracket" style="color: #FFFFFF;"></i> Cerrar Sesi&oacuten</a>
+                <a class="nav-link text-blue"  href="<?= REPORTE ?>"style="color:#FFFFFF"><i class="fa-regular fa-file" style="color: #FFFFFF;"></i> Reporte</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-blue"  href="<?= LOGOUT ?>" style="color:#FFFFFF"> <i class="fa-solid fa-right-from-bracket" style="color: #FFFFFF;"></i> Cerrar Sesi&oacuten</a>
             </li>
         </ul>
     </nav>
---
+++ Views/reporteExamenes.php
@@ -0,0 +1,84 @@
+<?php
+$directorioActual = __DIR__;
+$rutareporte = dirname($directorioActual) . "../Controllers/reportesController.php";
+$rutacceso = dirname($directorioActual) . "../Controllers/accesoController.php";
+require_once($rutacceso);
+require_once($rutareporte);
+$perfilesPermitidos = 5;
+verificarAcceso($perfilesPermitidos); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<link rel="icon" type="image/svg+xml" href="~/favicon.ico"/>
+	<link rel="stylesheet"
+		  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0"/>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="../css/nav.css">
+	<title>Diagnósticos</title>
+</head>
+<header class="navbar navbar-light fixed-top" style="background-color: #FFFFFF;">
+    <?php
+    include("../Views/Shared/nav.php");
+    ?>
+</header>
+
+<body>
+<section>
+        <h1 class='text-center' style="padding-top: 20px;">Exámenes por Centro Médico</h1><br>
+        <table id="tableUsers" class="tabla table">
+            <thead>
+                <tr>
+                    <th>Centro Médico</th>
+                    <?php 
+                    $totalFinal = 0;
+                    foreach ($examenes as $examen) { ?>
+                        <th><?php echo $examen; ?></th>
+                    <?php } ?>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($centrosMedicos as $centroMedico) { ?>
+                    <tr>
+                        <td><?php echo $centroMedico; ?></td>
+                        <?php
+                        $totalExamenesCentro = 0;
+
+                        foreach ($examenes as $examen) {
+                            $cantidad = $reporte->obtenerCantidadExamenesPorCentro($centroMedico, $examen);
+                            $totalExamenesCentro += $cantidad;
+                        }
+
+                        foreach ($examenes as $examen) {
+                            $cantidad = $reporte->obtenerCantidadExamenesPorCentro($centroMedico, $examen);
+                            $porcentaje = 0;
+                            if ($totalExamenesCentro > 0) {
+                                $porcentaje = ($cantidad / $totalExamenesCentro) * 100;
+                            }
+                        ?>
+                            <td>
+                                <?php echo $cantidad; ?>
+                                (<?php echo round($porcentaje,2); ?>%)
+                            </td>
+                        <?php } ?>
+                        <td><?php echo $totalExamenesCentro; 
+                        $totalFinal += $totalExamenesCentro;
+                        ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </section>
+    <h2>Total Exámenes: <?php echo $totalFinal?></h2>
+    <script src="https://kit.fontawesome.com/4652dbea50.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
