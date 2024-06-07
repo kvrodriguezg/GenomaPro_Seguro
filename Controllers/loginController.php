0b3fe5b4f991b120069d91c730c4f9ad9617d7df
diomarmajano <http.diomar@gmail.com>
2024-06-07 01:09:32 -0400

    modificcion de envio de correo

--- Controllers/loginController.php
+++ Controllers/loginController.php
@@ -75,79 +75,93 @@ function generarCodigo()
 
 function SendMail($correo, $codigo)
 {
-    $body = '<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Correo de Verificación de GenomaPro</title>
-                <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f8f8f8;
-                    margin: 0;
-                    padding: 0;
-                }
-                .container {
-                    max-width: 600px;
-                    margin: 20px auto;
-                    padding: 20px;
-                    background-color: #ffffff;
-                    border-radius: 10px;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                }
-                .header {
-                    text-align: center;
-                    margin-bottom: 20px;
-                }
-                .header h1 {
-                    color: #333333;
-                    margin: 0;
-                }
-                .code {
-                    background-color: #3498db;
-                    color: #ffffff;
-                    font-size: 24px;
-                    text-align: center;
-                    padding: 10px;
-                    border-radius: 5px;
-                    margin-bottom: 20px;
-                }
-                .footer {
-                    text-align: center;
-                    margin-top: 20px;
-                    color: #777777;
-                }
-                </style>
-                </head>
-                <body>
-                <div class="container">
-                <div class="header">
-                    <h1>Verificación en Dos Pasos - GenomaPro</h1>
-                </div>
-                <p>Hola,</p>
-                <p>Has solicitado un código de verificación para acceder a tu cuenta en GenomaPro. Por favor, utiliza el siguiente código para completar el proceso de verificación:</p>
-                <div class="code">' . $codigo . '</div>
-                <p>Por favor, no compartas este código con nadie. Si no has solicitado este código, ignora este correo electrónico.</p>
-                <p>Gracias,<br>El equipo de GenomaPro</p>
-                <div class="footer">
-                    <p>&copy; 2024 GenomaPro. Todos los derechos reservados.</p>
-                </div>
-                </div>
-                </body>
-                </html>';
-
-    $resend = Resend::client('re_2UDHyywR_3ikJ1D1Wj4sy5eebPonH9hXT');
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
 
     try {
-        $result = $resend->emails->send(array(
-            'from' => 'Acme <onboarding@resend.dev>',
-            'to' => array($correo),
-            'subject' => 'Codigo de verificacion',
-            'html' => $body,
-        ));
-    } catch (\Exception $e) {
-        exit('Error: ' . $e->getMessage());
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'http.diomar@gmail.com';
+        $mail->Password = 'vgoiwxjqlcqvrhcx';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('http.diomar@gmail.com', 'Diomar');
+        $mail->addAddress($correo);
+        $dir = 'C:/xampp/htdocs/Genoma/GenomaPro/img/adn4.jpeg';
+        $mail->AddEmbeddedImage($dir, 'imagen_cid', 'imagen.jpeg');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Código de ingreso.';
+        $body = '<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Correo de Verificación de GenomaPro</title>
+                    <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f8f8f8;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .container {
+                        max-width: 600px;
+                        margin: 20px auto;
+                        padding: 20px;
+                        background-color: #ffffff;
+                        border-radius: 10px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    }
+                    .header {
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .header h1 {
+                        color: #333333;
+                        margin: 0;
+                    }
+                    .code {
+                        background-color: #3498db;
+                        color: #ffffff;
+                        font-size: 24px;
+                        text-align: center;
+                        padding: 10px;
+                        border-radius: 5px;
+                        margin-bottom: 20px;
+                    }
+                    .footer {
+                        text-align: center;
+                        margin-top: 20px;
+                        color: #777777;
+                    }
+                    </style>
+                    </head>
+                    <body>
+                    <div class="container">
+                    <div class="header">
+                        <h1>Verificación en Dos Pasos - GenomaPro</h1>
+                    </div>
+                    <p>Hola,</p>
+                    <p>Has solicitado un código de verificación para acceder a tu cuenta en GenomaPro. Por favor, utiliza el siguiente código para completar el proceso de verificación:</p>
+                    <div class="code">' . $codigo . '</div>
+                    <p>Por favor, no compartas este código con nadie. Si no has solicitado este código, ignora este correo electrónico.</p>
+                    <img src="cid:imagen_cid" alt="Imagen">
+                    <p>Gracias,<br>El equipo de GenomaPro</p>
+                    <div class="footer">
+                        <p>&copy; 2024 GenomaPro. Todos los derechos reservados.</p>
+                    </div>
+                    </div>
+                    </body>
+                    </html>';
+        $mail->Body = $body;
+
+        $mail->send();
+        echo 'Correo enviado con éxito';
+    } catch (Exception $e) {
+        echo "No se pudo enviar el correo. Error de Mailer: {$mail->ErrorInfo}";
     }
 }
 ?>
